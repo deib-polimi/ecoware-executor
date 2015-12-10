0d8addf0f238e9eb14df3d01bd2184855386e7cb
Dmitrii Stebliuk <dsteblyuk@gmail.com>
2015-12-10 23:07:02 +0100

    Sorting the topology representation

--- httpHandler.py
+++ httpHandler.py
@@ -5,9 +5,12 @@ import SocketServer
 import json
 import topologyManager
 import traceback
+from collections import OrderedDict
 from traceback import print_exc
 from sys import argv
 from translator import Translator
+
+
 
 class HttpHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
   def do_GET(self):
@@ -25,12 +28,11 @@ class HttpHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
     return self.routing('post')
 
   def routing(self, method):
-
     body = ''
     try:
       if method == 'get':
         if self.path == '/api/topology':
-          body = json.dumps(topologyManager.get_current())
+          body = json.dumps(custom_sort(topologyManager.get_current()))
       elif method == 'post':
         post_data_string = self.rfile.read(int(self.headers['Content-Length']))
         post_data = json.loads(post_data_string)
@@ -48,10 +50,10 @@ class HttpHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
           body = json.dumps(string_actions)
         elif self.path == '/api/plan/preview':
           preview = topologyManager.preview(actions)
-          body = json.dumps(preview)
+          body = json.dumps(custom_sort(preview))
         elif self.path == '/api/plan/execute':
           new_topology = topologyManager.execute(actions)
-          body = json.dumps(new_topology)
+          body = json.dumps(custom_sort(new_topology))
       self.send_response(200)
       self.send_header('Content-Type', 'application/json')
       self.end_headers()
@@ -62,6 +64,13 @@ class HttpHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
       self.end_headers()
 
 if (__name__ == '__main__'):
+
+  def custom_sort(dic):
+    for key in dic:
+      if isinstance(dic[key], dict):
+        dic[key] = custom_sort(dic[key])
+    return OrderedDict(sorted(dic.items(), key=lambda t: t[0]))
+
   listen_ip = '0.0.0.0'
   if len(argv) >= 2:
     port = int(argv[1])
