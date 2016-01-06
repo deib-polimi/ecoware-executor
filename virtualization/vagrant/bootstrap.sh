2c6bd556c74fc9d41dc1920b37c9ba178d2f0d20
Dmitrii Stebliuk <dsteblyuk@gmail.com>
2016-01-06 13:39:24 +0100

    Add vagrant virtual machine scripts

---
+++ virtualization/vagrant/bootstrap.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+echo color_prompt=yes >> /home/vagrant/.bashrc
+echo "alias docker-set=docker exec -it docker-set docker" >> /home/vagrant/.bashrc
+echo "alias swarm=docker run -it --rm --privileged n43jl/swarm-set" >> /home/vagrant/.bashrc
+
