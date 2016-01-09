61935240b717c740958b452c7ad43aa44836906f
Dmitrii Stebliuk <dsteblyuk@gmail.com>
2016-01-09 21:02:23 +0100

    Fix docker config file

--- virtualization/vagrant/bootstrap.sh
+++ virtualization/vagrant/bootstrap.sh
@@ -3,3 +3,4 @@ echo color_prompt=yes >> /home/vagrant/.bashrc
 echo "alias docker-set='docker exec -it docker-set docker'" >> /home/vagrant/.bashrc
 echo "alias swarm='docker run -it --rm --privileged n43jl/swarm-set'" >> /home/vagrant/.bashrc
 cp /vagrant/docker /etc/default/docker
+service docker restart
