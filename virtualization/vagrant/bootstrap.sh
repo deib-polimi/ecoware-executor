99ecc637ec8ef3d8cf8ad6c1c15e7f45c2200d7e
Dmitrii Stebliuk <dsteblyuk@gmail.com>
2016-01-11 01:45:10 +0100

    Fix vagrant script

--- virtualization/vagrant/bootstrap.sh
+++ virtualization/vagrant/bootstrap.sh
@@ -4,3 +4,4 @@ echo "alias docker-set='docker -H :2376 exec -it docker-set docker'" >> /home/va
 # echo "alias swarm='docker -H :2376 run -it --rm --privileged n43jl/swarm-set'" >> /home/vagrant/.bashrc
 cp /vagrant/docker /etc/default/docker
 service docker restart
+docker -H 2376 run -it --privileged -d --name=docker-set n43jl/docker-set
