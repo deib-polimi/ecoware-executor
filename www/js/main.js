ccc71889614c29465d0fea1eae44ae086df921ff
Dmitrii Stebliuk <dsteblyuk@gmail.com>
2016-01-23 17:43:29 +0100

    Add VM form validation

--- www/js/main.js
+++ www/js/main.js
@@ -12,4 +12,27 @@ $(document).ready(function () {
       $row.appendTo($('#vms'));
     });
   });
+
+  $('#create-vm-btn').click(function() {
+    var isValid = $('#add-vm-form')[0].checkValidity();
+    if (!isValid) {
+      $('#add-vm-form').find(':submit').click();
+    } else {
+      var $btn = $(this);
+      $btn.button('loading');
+      var data = {
+        name: $('#vm-name-input').val(),
+        cpu_cores: $('#vm-cpu-input').val(),
+        mem_units: $('#vm-mem-input').val()
+      };
+      $.post('/api/vm/create', data, function(resp) {
+        toastr.success('VM {} is created (time={}s'.format(data.name, 60));
+        $btn.button('reset');
+      }).fail(function() {
+        toastr.error('Error creating VM')
+        console.error('Error creating VM')
+        $btn.button('reset');
+      });
+    }
+  });
 });
