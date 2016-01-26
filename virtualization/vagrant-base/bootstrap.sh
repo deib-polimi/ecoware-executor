313a99608331997ba717f2c39a6fe4dc0d4bec42
Dmitrii Stebliuk <dsteblyuk@gmail.com>
2016-01-26 22:30:01 +0100

    Fix and add folder mounting to vagrant-base boostrap script

--- virtualization/vagrant-base/bootstrap.sh
+++ virtualization/vagrant-base/bootstrap.sh
@@ -4,6 +4,6 @@ echo "alias docker-set='docker -H :2376 exec -it docker-set docker'" >> /home/va
 # echo "alias swarm='docker -H :2376 run -it --rm --privileged n43jl/swarm-set'" >> /home/vagrant/.bashrc
 cp /vagrant/docker /etc/default/docker
 service docker restart
-docker -H 2376 run -it --privileged -d --name=docker-set n43jl/docker-set
-docker -H 2376 exec docker-set docker pull nginx
-docker -H 2376 exec docker-set docker pull apache2
+docker -H :2376 run -it --privileged -d -v=/vagrant:/vagrant --name=docker-set n43jl/docker-set
+docker -H :2376 exec docker-set docker pull nginx
+docker -H :2376 exec docker-set docker pull httpd
