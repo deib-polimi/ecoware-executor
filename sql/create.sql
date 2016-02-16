ad278524d50f157af177fb10f6f1a34f5d8a08c2
Dmitrii Stebliuk <dsteblyuk@gmail.com>
2016-02-16 23:17:44 +0100

    Remove duplication of scale_hook table in create script

--- sql/create.sql
+++ sql/create.sql
@@ -18,14 +18,6 @@ create table container (
   foreign key (vm_id) references vm (id)
 );
 
-create table scale_hook (
-  id integer primary key,
-  container_id integer not null,
-  hook varchar(255) not null,
-  unique(container_id, hook),
-  foreign key (container_id) references container (id)
-);
-
 create table db_version (
   id integer primary key,
   version integer not null
