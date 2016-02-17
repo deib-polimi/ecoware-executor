27ced685077780ae8637b765a091e567fc8235a3
Dmitrii Stebliuk <dsteblyuk@gmail.com>
2016-02-18 00:42:42 +0100

    Changing Vagrant and docker files

--- virtualization/docker/executor/run.sh
+++ virtualization/docker/executor/run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 git clone https://github.com/n43jl/executor
 cd executor
+./init.sh
 ./httpHandler.py
--- virtualization/vagrant-base/bootstrap.sh
+++ virtualization/vagrant-base/bootstrap.sh
@@ -1,9 +1,4 @@
 #!/usr/bin/env bash
-echo color_prompt=yes >> /home/vagrant/.bashrc
-curl -fsSL https://test.docker.com/ | sh   # download v1.10_rc2
+sed -i '/^#force_color_prompt=yes/s/^#//' /home/vagrant/.bashrc
 usermod -aG docker vagrant
-docker pull nginx
-docker pull httpd
-echo 'DOCKER_OPTS="-H tcp://0.0.0.0:2376"' >> /etc/default/docker
-service docker restart
-echo "alias dock='docker -H :2376'" >> /home/vagrant/.bashrc
+    
