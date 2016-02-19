7b9d6f63ef303b2e09fbad9d40444d5e77173512
Dmitrii Stebliuk <dsteblyuk@gmail.com>
2016-02-19 22:59:12 +0100

    Change awscli to HTTP REST call

--- aws_driver.py
+++ aws_driver.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python
 import subprocess
+import sys, os, base64, datetime, hashlib, hmac 
+import urllib
+import requests # pip install requests
+from collections import OrderedDict
 
-def set_desired_capacity(capacity):
+def cli_set_desired_capacity(capacity):
   cmd = 'aws --region us-west-2 autoscaling update-auto-scaling-group --auto-scaling-group-name ex3 --desired-capacity {0} --max {0}'.format(capacity)
   try:
     subprocess.check_output(cmd, stderr=subprocess.STDOUT, shell=True)
@@ -9,3 +13,78 @@ def set_desired_capacity(capacity):
   except subprocess.CalledProcessError, ex: # error code <> 0 
     print ex.output
     raise Exception(ex.output)
+
+def set_desired_capacity(capacity):
+  method = 'GET'
+  endpoint = 'https://autoscaling.us-west-2.amazonaws.com'
+  parameters = {
+    'Action': 'UpdateAutoScalingGroup',
+    'AutoScalingGroupName': 'ex3',
+    'DesiredCapacity': capacity,
+    'HonorCooldown': False,
+    'MaxSize': capacity,
+    'Version': '2011-01-01'
+  }
+  request_url, headers = build_canonical_request(method, endpoint, parameters)
+  r = requests.get(request_url, headers=headers)
+  if r.status_code is not 200:
+    raise Exception(r.text)  
+  return r.text
+
+
+# http://docs.aws.amazon.com/general/latest/gr/sigv4-signed-request-examples.html
+# Key derivation functions. See:
+# http://docs.aws.amazon.com/general/latest/gr/signature-v4-examples.html#signature-v4-examples-python
+def sign(key, msg):
+    return hmac.new(key, msg.encode('utf-8'), hashlib.sha256).digest()
+
+def getSignatureKey(key, dateStamp, regionName, serviceName):
+    kDate = sign(('AWS4' + key).encode('utf-8'), dateStamp)
+    kRegion = sign(kDate, regionName)
+    kService = sign(kRegion, serviceName)
+    kSigning = sign(kService, 'aws4_request')
+    return kSigning
+
+def get_credentials():
+  access_key = os.environ.get('AWS_ACCESS_KEY_ID')
+  secret_key = os.environ.get('AWS_SECRET_ACCESS_KEY')
+  if access_key is None or secret_key is None:
+      msg = 'No access key is available.'
+      raise Exception(msg)
+  return (access_key, secret_key)
+
+def get_date():
+  # Create a date for headers and the credential string
+  t = datetime.datetime.utcnow()
+  amzdate = t.strftime('%Y%m%dT%H%M%SZ')
+  datestamp = t.strftime('%Y%m%d') # Date w/o time, used in credential scope
+  return (amzdate, datestamp)
+
+def build_canonical_request(method, endpoint, parameters):
+  access_key, secret_key = get_credentials()
+  ordered_parameters = OrderedDict(sorted(parameters.items()))
+  request_parameters = urllib.urlencode(ordered_parameters)
+  canonical_uri = '/' 
+  canonical_querystring = request_parameters
+  amzdate, datestamp = get_date()
+  # endpoint = 'https://autoscaling.us-west-2.amazonaws.com'
+  # host = 'autoscaling.us-west-2.amazonaws.com'
+  host = endpoint.split('//')[1]
+  canonical_headers = 'host:' + host + '\n' + 'x-amz-date:' + amzdate + '\n'
+  signed_headers = 'host;x-amz-date'
+  payload_hash = hashlib.sha256('').hexdigest()
+  canonical_request = method + '\n' + canonical_uri + '\n' + canonical_querystring + '\n' + canonical_headers + '\n' + signed_headers + '\n' + payload_hash
+  algorithm = 'AWS4-HMAC-SHA256'
+
+  # region = 'us-west-2'
+  region = host.split('.')[1]
+  # service = 'autoscaling'
+  service = host.split('.')[0]
+  credential_scope = datestamp + '/' + region + '/' + service + '/' + 'aws4_request'
+  string_to_sign = algorithm + '\n' +  amzdate + '\n' +  credential_scope + '\n' +  hashlib.sha256(canonical_request).hexdigest()
+  signing_key = getSignatureKey(secret_key, datestamp, region, service)
+  signature = hmac.new(signing_key, (string_to_sign).encode('utf-8'), hashlib.sha256).hexdigest()
+  authorization_header = algorithm + ' ' + 'Credential=' + access_key + '/' + credential_scope + ', ' +  'SignedHeaders=' + signed_headers + ', ' + 'Signature=' + signature
+  headers = {'x-amz-date':amzdate, 'Authorization':authorization_header}
+  request_url = endpoint + '?' + canonical_querystring
+  return (request_url, headers)
